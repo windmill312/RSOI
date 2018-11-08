8fce29715bb153475c52e35ad01cc61896c80a8b
andrey <windmill5667@gmail.com>
2018-11-09 01:18:17 +0300

    Add tabs

--- frontend/react-ui/src/App.js
+++ frontend/react-ui/src/App.js
@@ -1,112 +1,182 @@
 import React from 'react'
 import './App.css'
 import axios from 'axios'
-import {Button, Table} from 'reactstrap'
+import {Table, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col, Jumbotron} from 'reactstrap'
+import classnames from 'classnames';
 import 'bootstrap/dist/css/bootstrap.css'
-import 'react-bootstrap-table/css/react-bootstrap-table.css'
 
 class App extends React.Component {
 
     constructor() {
         super()
         this.state = {
-            tickets: []
+            tickets: [],
+            activeTab: '0',
+            disabled : false
         }
-        this.handleClick = this.handleClick.bind(this)
-
     }
 
-    dropdown() {
-        document.getElementById("tableId").style.display = "inline-block";
-    }
-
-    handleClick() {
+    componentDidMount() {
         axios.get('http://localhost:8090/tickets')
             .then(
-                response => this.setState({tickets: response.data}
+                response => this.setState({
+                        tickets: response.data
+                    }
                 ))
             .catch((error) => {
                 console.error(error);
             });
-
-        this.dropdown();
     }
 
-    deleteTicket() {
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
+
+    onOpen() {
+        this.setState({
+            activeTab: 1,
+            disabled: true
+        })
+    }
+
+    changeTicket(idTicket, classType) {
+        console.log("changed")
+    }
+
+    deleteTicket(idTicket) {
         console.log("deleted")
     }
 
     render() {
         return (
-            < div
-        className = "main" >
-            < div
-        className = 'button__container' >
-            < Button
-        className = 'button'
-        onClick = {this.handleClick
-    }>
-        Get
-        Tickets
-        < /Button>
-        < /div>
 
-        < div
-        className = "content_container" >
-            < Table
-        id = "tableId"
-        className = "table" >
-            < thead >
-            < tr >
-            < th > Ticket
-        ID < /th>
-        < th > Flight
-        ID < /th>
-        < th > Passenger
-        ID < /th>
-        < th > Class < /th>
-        < th > UUID < /th>
-        < /tr>
-        < /thead>
-        < tbody >
-        {this.state.tickets.map(function (ticket) {
-            return ( < tr >
-                < th
-            key = {ticket.idTicket
-        }>
-            {
-                ticket.idTicket
-            }
-        <
-            /th>
-            < td > {ticket.idFlight
-        }<
-            /td>
-            < td > {ticket.idPassenger
-        }<
-            /td>
-            < td > {ticket.classType
-        }<
-            /td>
-            < td > {ticket.uid
-        }<
-            /td>
-            < Button
-            className = "deleteButton"
-            onClick = {this.deleteClick.bind(this, ticket.idTicket)
-        }>
-            Delete
-            < /Button>
-            < /tr>
+            <div>
+                <Nav tabs>
+                    <NavItem>
+                        <NavLink disabled={this.state.disabled} id="authTab"
+                                 className={classnames({ active: this.state.activeTab === '0' })}
+                        >
+                            Авторизация
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink  id="ticketTab"
+                            className={classnames({ active: this.state.activeTab === '1' })}
+                                onClick={() => {this.toggle('1')}}
+                        >
+                            Билеты
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '2' })}
+                                 onClick={() => { this.toggle('2')}}
+                        >
+                            Рейсы
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink
+                            className={classnames({ active: this.state.activeTab === '3' })}
+                            onClick={() => { this.toggle('3')}}
+                        >
+                            Маршруты
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent activeTab={this.state.activeTab}>
+
+                    <TabPane tabId="0">
+                        <Jumbotron>
+                            <h1 className="display-3">Привет, Пользователь!</h1>
+                            <p className="lead">Это приветственное окно. В ближайшем будущем здесь будет авторизация</p>
+                            <hr className="my-2" />
+                            <p>Для продолжения нажми кнопку Далее</p>
+                            <p className="lead">
+                                <Button color="primary" onClick={() => {this.onOpen()}}>Далее</Button>
+                            </p>
+                        </Jumbotron>
+                    </TabPane>
+
+                    <TabPane tabId="1">
+                        <Row>
+                            <Col sm="12">
+                                <div className="content_container">
+                                    <Table id = "tableId" className = "table">
+                                        <thead>
+                                        <tr>
+                                            <th> Ticket ID </th>
+                                            <th> Flight ID </th>
+                                            <th> Passenger ID </th>
+                                            <th> Class </th>
+                                            <th> UUID </th>
+                                            <th></th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {this.state.tickets.map(function (ticket) {
+                                            return (
+                                                <tr>
+                                                    <th key = {ticket.idTicket}> {ticket.idTicket}</th>
+                                                    <td> {ticket.idFlight}</td>
+                                                    <td contentEditable={true} onChange={() => this.changeTicket()}> {ticket.idPassenger}</td>
+                                                    <td> {ticket.classType}</td>
+                                                    <td> {ticket.uid}</td>
+                                                    <td><Button color="danger" onClick={() => this.deleteTicket(ticket.idTicket)}>Удалить</Button></td>
+                                                </tr>
+                                            )
+                                        })}
+                                        </tbody>
+                                    </Table>
+                                </div>
+                            </Col>
+                        </Row>
+                    </TabPane>
+
+                    <TabPane tabId="2">
+                        <Row>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </TabPane>
+
+                    <TabPane tabId="3">
+                        <Row>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                            <Col sm="6">
+                                <Card body>
+                                    <CardTitle>Special Title Treatment</CardTitle>
+                                    <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                    <Button>Go somewhere</Button>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </TabPane>
+                </TabContent>
+            </div>
         )
-        })
-    }
-    <
-        /tbody>
-        < /Table>
-        < /div>
-        < /div>
-    )
     }
 }
 
