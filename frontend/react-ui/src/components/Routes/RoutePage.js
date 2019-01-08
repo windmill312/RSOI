c50897125770d1cc71fc61561c8c3777637ee441
andrey <windmill312@mail.ru>
2019-01-09 00:29:04 +0300

    Fix IDEA fucking removing error (React)

--- frontend/react-ui/src/components/Routes/RoutePage.js
+++ frontend/react-ui/src/components/Routes/RoutePage.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import RouteForm from './RouteForm';
+import {pingRoutes, countRoutes, getRoutes, createRoute, deleteRoute, getTicketsAndFlights} from '../../actions/RoutesActions';
+import PropTypes from "prop-types";
+import connect from "react-redux/es/connect/connect";
 
 class RoutePage extends React.Component {
     render() {
+        const {pingRoutes, countRoutes, getRoutes, createRoute, deleteRoute, getTicketsAndFlights} = this.props;
         return (
             <div>
-                <RouteForm />
+                <RouteForm
+                    pingRoutes={pingRoutes}
+                    countRoutes={countRoutes}
+                    createRoute={createRoute}
+                    deleteRoute={deleteRoute}
+                    getRoutes={getRoutes}
+                    getTicketsAndFlights={getTicketsAndFlights}
+                />
             </div>
         );
     }
 }
 
-export default RoutePage;
+RouteForm.propTypes = {
+    pingRoutes: PropTypes.func.isRequired,
+    countRoutes: PropTypes.func.isRequired,
+    getRoutes: PropTypes.func.isRequired,
+    createRoute: PropTypes.func.isRequired,
+    deleteRoute: PropTypes.func.isRequired,
+    getTicketsAndFlights: PropTypes.func.isRequired
+};
+
+export default connect(null, { pingRoutes, countRoutes, getRoutes, createRoute, deleteRoute, getTicketsAndFlights})(RoutePage);
